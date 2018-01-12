38f0ddb4aa98f3daff39616ca8cc4edbc5054f55
juanda <juanda@juandarodriguez.es>
2018-01-12 13:30:51 +0100

    funcionando

--- renderer.js
+++ renderer.js
@@ -1,3 +1,20 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+$( document ).ready(function() {
+    $('#btn_qr').click(function(){
+        var text_data = {texto: $('#texto').val()}    
+        $.ajax({
+            url:"http://localhost:5000/generate-qr",
+            type:"POST",
+            data:JSON.stringify(text_data),
+            contentType:"application/json; charset=utf-8",
+            dataType:"json",
+            success: function(data){
+                d = new Date();
+                $('#qrimage').attr("src",data.url+ "?"+d.getTime());
+            },
+            error: function(data){
+                $('#result').text('Fallo al consultar la API');
+            }
+          })
+
+    });
+  });
