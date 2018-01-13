1b1dfb50648036ac3b3fc16ee5544f302e180e93
Juanda Rodríguez <juandalibaba@gmail.com>
2018-01-13 10:47:24 +0100

    se añade la funcionalidad de guardar el código qr a fichero

--- renderer.js
+++ renderer.js
@@ -1,20 +1,43 @@
-$( document ).ready(function() {
-    $('#btn_qr').click(function(){
-        var text_data = {texto: $('#texto').val()}    
+const { dialog } = require('electron').remote;
+var http = require('http');
+var fs = require('fs');
+
+$(document).ready(function () {
+    var url_qr = '';
+    $('#btn_qr').click(function () {
+
+        var text_data = { texto: $('#texto').val() }
         $.ajax({
-            url:"http://localhost:5000/generate-qr",
-            type:"POST",
-            data:JSON.stringify(text_data),
-            contentType:"application/json; charset=utf-8",
-            dataType:"json",
-            success: function(data){
+            url: "http://localhost:5000/generate-qr",
+            type: "POST",
+            data: JSON.stringify(text_data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                url_qr = data.url;
                 d = new Date();
-                $('#qrimage').attr("src",data.url+ "?"+d.getTime());
+                $('#qrimage').attr("src", data.url + "?" + d.getTime());
             },
-            error: function(data){
+            error: function (data) {
                 $('#result').text('Fallo al consultar la API');
             }
-          })
-
+        })
     });
-  });
+
+    $('#btn_save').click(function () {
+        if(url_qr === ''){
+            dialog.showMessageBox({title: 'Atención', message: 'Primero hay que generar el código'});
+            return;
+        }
+        var fichero = dialog.showSaveDialog(
+            {},
+            function (filepath) {
+                var file = fs.createWriteStream(filepath);
+                var request = http.get(url_qr, function (response) {
+                    response.pipe(file);
+                    dialog.showMessageBox({title: 'Información', message: 'La imagen con el código QR ha sido guardada correctamente'});
+                });
+            }
+        )
+    });
+});
